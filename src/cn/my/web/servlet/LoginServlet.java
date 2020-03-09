3618ab4192409c61685de65cf6e51e32561a7fa3
TQBX <hyh1332790762@163.com>
2020-03-09 12:55:55 +0800

    " 1.新导入commons-beanutils包   2.采用BeanUtils包下的populate封装对象 "

--- src/cn/my/web/servlet/LoginServlet.java
+++ src/cn/my/web/servlet/LoginServlet.java
@@ -2,6 +2,7 @@ package cn.my.web.servlet;
 
 import cn.my.dao.UserDao;
 import cn.my.domain.User;
+import org.apache.commons.beanutils.BeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 /**
  * @auther Summerday
@@ -16,14 +19,14 @@ import java.io.IOException;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request,response);
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //1.设置编码
         request.setCharacterEncoding("utf-8");
 
-        //2.获取请求参数
+        /*2.获取请求参数
         String username = request.getParameter("username");
 
         String password = request.getParameter("password");
@@ -31,28 +34,35 @@ public class LoginServlet extends HttpServlet {
         //3.封装User对象
         User loginUser = new User();
         loginUser.setUsername(username);
-        loginUser.setPassword(password);
+        loginUser.setPassword(password);*/
 
-        User user = null;
+        //获取所有请求参数
+        Map<String, String[]> map = request.getParameterMap();
+        //创建User对象
+        User loginUser = new User();
+
+        //使用BeanUtils封装
         try {
-            //4.调用UserDao的login方法
-            UserDao userDao = new UserDao();
-            user = userDao.login(loginUser);
-            System.out.println(user);
-        }catch (Exception e){
-            System.out.println(e.getMessage());
+            BeanUtils.populate(loginUser,map);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
         }
 
+        //4.调用UserDao的login方法
+        UserDao userDao = new UserDao();
+        User user = userDao.login(loginUser);
+
+
         //5.判断user
-        if(user == null){
+        if (user == null) {
             //登录失败
-            request.getRequestDispatcher("/failServlet").forward(request,response);
-        }else {
+            request.getRequestDispatcher("/failServlet").forward(request, response);
+        } else {
             //登陆成功
             //存储数据
-            request.setAttribute("user",user);
+            request.setAttribute("user", user);
             //转发
-            request.getRequestDispatcher("/successServlet").forward(request,response);
+            request.getRequestDispatcher("/successServlet").forward(request, response);
         }
 
 
